8637f08521bb5e420f768ef9d29633f0e6c74ade
Alex Markowitz <Alex@alexs-mbp-3.lan>
2020-03-05 03:20:20 -0500

    more fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import "./__styles__/App.css";
-import Home from "./Home";
-import Header from "./Header";
-import Footer from "./Footer";
-import Notes from "./Notes";
-import Note from "./Note";
+import './__styles__/App.css';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+import Notes from './Notes';
+import Note from './Note';
 
 export default function App() {
   return (
